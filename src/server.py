8a79c7fd8a51feb2bdd069d4e51c6dd2bb3aaf2f
Kraken-coder <diptanshukumar9471@gmail.com>
2025-12-29 19:59:10 +0530

    Cors all origin

--- src/server.py
+++ src/server.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import pickle
 from schemas import CropPriceFeatures
 import pandas as pd
+
 app = FastAPI()
+
+# Allow all origins for CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 with open("xgb_model.pkl", "rb") as f:
     loaded_xgb_model = pickle.load(f)
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
@@ -13,5 +26,4 @@ def read_root():
 def predict(input_data: CropPriceFeatures):
     input_df = pd.DataFrame([input_data.model_dump(by_alias=True)])
     prediction = loaded_xgb_model.predict(input_df)
-    
     return {"predicted_price": prediction.tolist()}
